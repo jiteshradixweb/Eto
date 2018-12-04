1e76f03883d5817b02446f444b7fcac8952260c8
Jitesh Kala <jitesh@radixweb.com>
2018-12-04 20:05:05 +0530

    Used GtkShrinkableVBox in panel, because Gtk.Vbox not allowing resize.

--- src/Eto.Gtk/Forms/Controls/PanelHandler.cs
+++ src/Eto.Gtk/Forms/Controls/PanelHandler.cs
@@ -4,13 +4,16 @@ namespace Eto.GtkSharp.Forms.Controls
 {
 	public class PanelHandler : GtkPanel<Gtk.EventBox, Panel, Panel.ICallback>, Panel.IHandler
 	{
-		readonly Gtk.VBox box;
+		//readonly Gtk.VBox box;
+		readonly GtkShrinkableVBox box;
 
 		public PanelHandler()
 		{
 			Control = new Gtk.EventBox();
 			//Control.VisibleWindow = false; // can't use this as it causes overlapping widgets
-			box = new Gtk.VBox();
+			//box = new Gtk.VBox();
+			box = new GtkShrinkableVBox();
+			box.Resizable = true;
 			Control.Add(box);
 		}
 
